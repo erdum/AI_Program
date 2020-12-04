7d0dfc04c4e9a8cf86a711885abe0876dea14262
Erdum <erdumadnan@gmail.com>
2020-12-04 22:57:54 +0500

    Update flashControl.ino

--- flashControl.ino
+++ flashControl.ino
@@ -8,10 +8,10 @@ void setup(){
 void loop(){
     duration = analogRead(potPin);
     // Here i am mapping 1024 analog value to 1000 milliseconds 
-    mapped_value = (1000 / 1024) * duration;
+    int mapped_value = (1000 / 1024) * duration;
     // we can also use map function here mapped_value = map(duration, 0, 1024, 0, 1000);
     digitalWrite(ledPin, HIGH);
-    delay(duration);
+    delay(mapped_value);
     digitalWrite(ledPin, LOW);
-    delay(duration);
+    delay(mapped_value);
 }
